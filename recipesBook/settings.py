ca8f815a458d9865f058b23853f40ccf2287dd34
Sevenamber <sevenamber@gmail.com>
2022-02-19 22:08:23 +0300

    fix: BASEDIR

--- recipesBook/settings.py
+++ recipesBook/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "static"
 
-# BASE_DIR.joinpath("static")
+
